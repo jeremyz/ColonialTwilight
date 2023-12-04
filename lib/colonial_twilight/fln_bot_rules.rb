5b76f11bb327fcc4760e89e0c86ec42083049155
Jérémy Zurcher <jeremy@asynk.ch>
2023-12-04 14:45:28 +0100

    FLNBotRules : clean up

--- lib/colonial_twilight/fln_bot_rules.rb
+++ lib/colonial_twilight/fln_bot_rules.rb
@@ -151,8 +151,9 @@ module ColonialTwilight
     end
 
     def rally_9_priority(spaces, resources, &is_rallied)
-      f = _filter(spaces) { |s| s.support? && (resources.zero? || (resources - (is_rallied.call(s) ? 0 : 1)) > s.terror) }
-      _filter(f) { |s| s.neutral? && (resources.zero? || (resources - (is_rallied.call(s) ? 0 : 1)) > s.terror) }
+      has_resources = ->(s) { resources.zero? || (resources - (is_rallied.call(s) ? 0 : 1)) > s.terror }
+      f = _filter(spaces) { |s| s.support? && has_resources.call(s) }
+      _filter(f) { |s| s.neutral? && has_resources.call(s) }
     end
 
     # Extort
