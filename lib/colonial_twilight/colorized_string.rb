9d362a97a4189a6a7685e46339786c80f9fb02f3
Jérémy Zurcher <jeremy@asynk.ch>
2026-03-11 13:40:32 +0100

    fix files header

--- lib/colonial_twilight/colorized_string.rb
+++ lib/colonial_twilight/colorized_string.rb
@@ -1,4 +1,3 @@
-#! /usr/bin/env ruby
 # frozen_string_literal: true
 
 # this adds ascii colorization to String class
--- lib/colonial_twilight/gov_player.rb
+++ lib/colonial_twilight/gov_player.rb
@@ -1,4 +1,4 @@
-#! /usr/bin/env ruby
+# frozen_string_literal: true
 
 module ColonialTwilight
   class GOVPlayer < Player
